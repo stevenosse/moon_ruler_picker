e797b5874ca85aca6038dce7f98ad027fa992043
icecrao2 <icecrao2@daum.net>
2024-03-12 14:01:00 +0900

    readme edited

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-
-
 this package provide you with good ruler picker ui.
+
+## Description
+
+
+## 
+![](https://velog.velcdn.com/images/icecrao2/post/ae8d53d6-1bb0-4b3e-8e93-27e797192f9d/image.gif)
--- lib/ruler_picker_lib.dart
+++ lib/ruler_picker_lib.dart
@@ -5,9 +5,3 @@ library ruler_picker_lib;
 
 export 'ruler_picker/ruler_picker.dart' show MoonRulerPicker;
 
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
